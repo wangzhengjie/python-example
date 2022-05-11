17c28e7f35481d5ce51ebab7abb2bde7f8d58253
wangzhengjie <xcoortir@gmail.com>
2022-05-11 21:06:04 +0800

    airtw_epa

---
+++ airtw_epa.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+def airtw_epa():
+    URL = 'https://airtw.epa.gov.tw/CHT/Query/Month_Avg.aspx'
+    resp = requests.get(URL)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    view_state = soup.find(id='__VIEWSTATE')['value']
+    event_validation = soup.find(id='__EVENTVALIDATION')['value']
+    viewstate_generator = soup.find(id='__VIEWSTATEGENERATOR')['value']
+    form_data = {
+        '__VIEWSTATE': view_state,
+        '__VIEWSTATEGENERATOR': viewstate_generator,
+        '__EVENTVALIDATION': event_validation,
+        'ctl00$CPH_Content$ddl_Site': '46',
+        'ctl00$CPH_Content$ddlQYear': '2022',
+        'ctl00$CPH_Content$btnQuery': '查詢'
+    }
+    resp = requests.post(URL, data=form_data)
+    soup = BeautifulSoup(resp.text, 'html.parser')
+    for row in soup.find_all('tr'):
+        print([content for content in row.stripped_strings])
+
+
+# def taqm_epa():
+#     URL = 'https://taqm.epa.gov.tw/taqm/tw/HourlyData.aspx'
+#     resp = requests.get(URL)
+#     soup = BeautifulSoup(resp.text, 'html5lib')
+#     view_state = soup.find(id='__VIEWSTATE')['value']
+#     event_validation = soup.find(id='__EVENTVALIDATION')['value']
+#     viewstate_generator = soup.find(id='__VIEWSTATEGENERATOR')['value']
+#     form_data = {
+#         '__VIEWSTATE': view_state,
+#         '__VIEWSTATEGENERATOR': viewstate_generator,
+#         '__EVENTVALIDATION': event_validation,
+#         'ctl04$lbSite': '56',
+#         'ctl04$lbParam': '4',
+#         'ctl04$txtDateS': '2018/05/01',
+#         'ctl04$txtDateE': '2018/05/31',
+#         'ctl04$btnQuery': '查詢即時值'
+#     }
+#     resp = requests.post(URL, data=form_data)
+#     soup = BeautifulSoup(resp.text, 'html5lib')
+#     for t in soup.find_all('table', 'TABLE_G'):
+#         print([s for s in t.stripped_strings])
+
+
+if __name__ == '__main__':
+    airtw_epa()
+    # taqm_epa() 網址已失效
